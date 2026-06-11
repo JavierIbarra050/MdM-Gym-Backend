dc7ff602c7c0b2f5195e8d5afe62aef1087cd66c
JavierIbarra050 <ibarra.161804@e.unavarra.es>
2026-06-11 11:30:39 +0200

    [#8] - Creada entidad workout y workoutSet

---
+++ src/Domain/Dashboard/Workout/WorkoutSet.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace Src\Domain\Dashboard\Workout;
+
+class WorkoutSet
+{
+    private int $id;
+
+    private string $exerciseName;
+
+    private int $weight;
+
+    private int $reps;
+
+    public function __construct(
+        int $id,
+        string $exerciseName,
+        int $weight,
+        int $reps)
+    {
+        $this->id = $id;
+        $this->exerciseName = $exerciseName;
+        $this->weight = $weight;
+        $this->reps = $reps;
+    }
+
+    public function getId(): int
+    {
+        return $this->id;
+    }
+
+    public function getExerciseName(): string
+    {
+        return $this->exerciseName;
+    }
+
+    public function getWeight(): int
+    {
+        return $this->weight;
+    }
+
+    public function getReps(): int
+    {
+        return $this->reps;
+    }
+}
