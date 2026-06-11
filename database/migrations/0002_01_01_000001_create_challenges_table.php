65160a82fd9d6628a61f6a57ffc82e5e13ff4175
Javier Ibarra <ibarra.161804@e.unavarra.es>
2026-06-11 23:32:17 +0200

    [#54] - Añadida migración DB (#59)

---
+++ database/migrations/0002_01_01_000001_create_challenges_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('challenges', function (Blueprint $table) {
+            $table->id();
+            $table->string('title');
+            $table->text('description')->nullable();
+            $table->decimal('current_weight', 10, 2)->default(0);
+            $table->decimal('objective_weight', 10, 2);
+            $table->string('status')->default('ACTIVE'); // ACTIVE, COMPLETED, INACTIVE
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('challenges');
+    }
+};
