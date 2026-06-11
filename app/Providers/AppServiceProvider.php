ebd89d1166197754d74b79300c63d0c11c0fd8a6
JavierIbarra050 <ibarra.161804@e.unavarra.es>
2026-06-11 23:26:16 +0200

    [#53] - Enlazar interfaces a respositorios

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,20 +3,26 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Src\Domain\Repositories\IChallengeRepository;
+use Src\Domain\Repositories\IWorkoutRepository;
+use Src\Infrastructure\Repositories\ChallengeRepositoryDB;
+use Src\Infrastructure\Repositories\WorkoutRepositoryDB;
 
 class AppServiceProvider extends ServiceProvider
 {
-    /**
-     * Register any application services.
-     */
     public function register(): void
     {
-        //
+        $this->app->bind(
+            IChallengeRepository::class,
+            ChallengeRepositoryDB::class
+        );
+
+        $this->app->bind(
+            IWorkoutRepository::class,
+            WorkoutRepositoryDB::class
+        );
     }
 
-    /**
-     * Bootstrap any application services.
-     */
     public function boot(): void
     {
         //
