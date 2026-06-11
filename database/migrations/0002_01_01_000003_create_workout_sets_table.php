e7301878e798be365d409cb3fa886e57d83333da
Javier Ibarra <ibarra.161804@e.unavarra.es>
2026-06-12 00:26:37 +0200

    [#21] - Crear AnalyticsRepository (#76)

--- database/migrations/0002_01_01_000003_create_workout_sets_table.php
+++ database/migrations/0002_01_01_000003_create_workout_sets_table.php
@@ -14,6 +14,7 @@ return new class extends Migration
         Schema::create('workout_sets', function (Blueprint $table) {
             $table->id();
             $table->foreignId('workout_id')->constrained()->onDelete('cascade');
+            $table->string('exercise_name');
             $table->decimal('weight', 8, 2);
             $table->integer('reps');
             $table->boolean('is_completed')->default(false);
