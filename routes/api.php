5615f668588eb7b9b306a4b4ef5711399d03713a
JavierIbarra050 <ibarra.161804@e.unavarra.es>
2026-06-12 01:07:41 +0200

    [#43] - Crear UpdateWorkoutSetController

--- routes/api.php
+++ routes/api.php
@@ -5,6 +5,7 @@ use App\Http\Controllers\AnalyticsController;
 use App\Http\Controllers\ChallengeContributionController;
 use App\Http\Controllers\DashboardController;
 use App\Http\Controllers\ExerciseSearchController;
+use App\Http\Controllers\UpdateWorkoutSetController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/dashboard', DashboardController::class);
@@ -12,3 +13,4 @@ Route::post('/challenges/{id}/contribute', ChallengeContributionController::clas
 Route::get('/analytics', AnalyticsController::class);
 Route::get('/exercises', ExerciseSearchController::class);
 Route::get('/workouts/active', ActiveWorkoutController::class);
+Route::put('/workouts/sets/{id}', UpdateWorkoutSetController::class);
